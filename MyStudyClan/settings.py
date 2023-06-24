a63bc0d10a0a707b56e39957302b74d7c3da2cf7
Ubuntu <mshangamakonde@gmail.comgit config --global user.name Mshanga Makonde>
2023-06-24 09:12:30 +0000

    MyStudyClan'

--- MyStudyClan/settings.py
+++ MyStudyClan/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'MyStudyClan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mystudyclan',
-#         'USER': 'mystudyclan',
-#         'PASSWORD': 'mystudyclan',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mystudyclan',
+        'USER': 'mystudyclan',
+        'PASSWORD': 'mystudyclan',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
