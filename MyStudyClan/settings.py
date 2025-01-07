f356cd353b75c15cf37a1b481993eb1802053ed2
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2025-01-07 10:19:42 +0300

    changing db to sqlite

--- MyStudyClan/settings.py
+++ MyStudyClan/settings.py
@@ -93,12 +93,8 @@ WSGI_APPLICATION = 'MyStudyClan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mystudyclan',
-        'USER': 'mystudyclan',
-        'PASSWORD': 'mystudyclan',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
